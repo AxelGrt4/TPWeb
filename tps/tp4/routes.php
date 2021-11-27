ff6301fc5ea4f1bb51c9fc5156ffde4093d1207d
AxelGrt4 <71839642+AxelGrt4@users.noreply.github.com>
2021-11-27 20:51:23 +0100

    ajouts de commentaires

--- tps/tp4/routes.php
+++ tps/tp4/routes.php
@@ -10,18 +10,13 @@ Flight::route('GET /register', function(){
 
 
 
-
-
-
-
-
 Flight::route('POST /register', function(){
     
     $db = Flight::get('db');
-    $messages=array();
+    $messages=array();                     //tableau récupérant les messages d'erreur 
 
     //récup donnéees POST
-    $data = Flight::request() -> data;
+    $data = Flight::request() -> data;     //Recupération des données passées en POST
 
     if(empty($data->Nom)){
         $messages['Nom'] = "Le nom doit etre rempli";
@@ -29,14 +24,14 @@ Flight::route('POST /register', function(){
     if(empty($data->Email)){
         $messages['Email'] = "L'adresse mail doit etre remplie";
     } else {
-        if(!filter_var($data->Email, FILTER_VALIDATE_EMAIL)){
+        if(!filter_var($data->Email, FILTER_VALIDATE_EMAIL)){   //Teste si l'adresse mail est valide, inutile si le type d'input du formulaire est "email"
             $messages['Email']="L'adresse mail est invalide";
         }
     }
     $email = $data->Email;
-    $stmt = $db->prepare("SELECT * FROM utilisateur WHERE Email =?");
+    $stmt = $db->prepare("SELECT * FROM utilisateur WHERE Email =?"); //Recherche dans la base si un utilisateur possède déjà l'adresse mail rentrée
     $stmt->execute([$email]);
-    $utilisateur = $stmt->fetch();
+    $utilisateur = $stmt->fetch();   //retourne faux si l'adresse mail ne se trouve pas dans la base, vrai sinon
 
     if($utilisateur){
         $messages['Email'] = "L'utilisateur existe déjà";    
@@ -59,21 +54,21 @@ Flight::route('POST /register', function(){
     }
 
 
-    if(count($messages) == 0){
+    if(count($messages) == 0){             //Si aucun message d'erreur
            Flight::redirect('/success');
-           $stmt = $db -> prepare("INSERT INTO utilisateur VALUES (:Nom, :Email, :Motdepasse, :Ville, :Pays)");
+           $stmt = $db -> prepare("INSERT INTO utilisateur VALUES (:Nom, :Email, :Motdepasse, :Ville, :Pays)"); //Insère les données POST dans la base de données
            $stmt->execute(array(
                ':Nom' => $_POST['Nom'],
                ':Email' => $_POST['Email'],
-               ':Motdepasse' => password_hash($_POST['Motdepasse'], PASSWORD_DEFAULT),
+               ':Motdepasse' => password_hash($_POST['Motdepasse'], PASSWORD_DEFAULT), //cryptage du mdp rentré par l'utilisateur
                ':Ville' => $_POST['Ville'],
                ':Pays' => $_POST['Pays']
            ));
-    } else{
+    } else{    //Si message d'erreur
         Flight::render('register.tpl', 
             array(
                 'messages' => $messages,
-                'data' => $_POST
+                'data' => $_POST  //Insère dans les champs d'input du formulaire, les données rentrées précédemment (sauf mdp)
             ));
     }
     
@@ -115,12 +110,12 @@ Flight::route('POST /login', function(){
     $stmt->execute([$email]);
     $utilisateur = $stmt->fetch();
 
-    if($utilisateur){
-        $passwd = $db->prepare("SELECT Motdepasse FROM utilisateur WHERE Email =?");
+    if($utilisateur){        //Si l'utilisateur existe
+        $passwd = $db->prepare("SELECT Motdepasse FROM utilisateur WHERE Email =?");       //Récupère le mot de passe crypté de l'utilisateur
         $passwd->execute([$email]);
-        $mdp = $passwd->fetchColumn();
+        $mdp = $passwd->fetchColumn();   
 
-        if(!password_verify($data->Motdepasse, $mdp)){
+        if(!password_verify($data->Motdepasse, $mdp)){        //Vérifie que le mot de passe entré et le mot de passe dans la base correspondent
             $messages['Motdepasse'] = "Le mot de passe ne correspond pas";
         }   
     } else {
@@ -131,22 +126,22 @@ Flight::route('POST /login', function(){
         $messages['Motdepasse'] = "Le mot de passe doit faire 8 caractères";
     }
 
-    if(count($messages) == 0){
+    if(count($messages) == 0){     //Si pas de message d'erreur
         
-        $infoUser = $db->prepare("SELECT Nom FROM utilisateur WHERE Email =?");
+        $infoUser = $db->prepare("SELECT Nom FROM utilisateur WHERE Email =?");   //Récupère le nom de l'utilisateur pour l'afficher
         $infoUser->execute([$email]);
         $res = $infoUser->fetchColumn();
-        $_SESSION['Nom'] = $res;
+        $_SESSION['Nom'] = $res;          //Initialise la session au nom de l'utilisateur
         $_SESSION['Email'] = $email;
-        if(!empty($_SESSION['Nom'])) {
+        if(!empty($_SESSION['Nom'])) {   //Si la session est initialisée
             Flight::redirect('/');
         }
         
     } else{
-     Flight::render('login.tpl', 
+     Flight::render('login.tpl', //Redirige sur la page login
          array(
              'messages' => $messages,
-             'data' => $_POST
+             'data' => $_POST 
          ));
  }
 
@@ -156,10 +151,10 @@ Flight::route('/profil', function(){
     if(!empty($_SESSION)){
         $db = Flight::get('db');
 
-        $profil = $db->prepare("SELECT Nom, Email, Pays, Ville FROM utilisateur WHERE Email=?");
+        $profil = $db->prepare("SELECT Nom, Email, Pays, Ville FROM utilisateur WHERE Email=?");   //Récupère toutes les infos de l'utilisateur connecté
         $profil->execute([$_SESSION['Email']]);
-        $profil = $profil->fetchAll(PDO::FETCH_ASSOC);
-        Flight::view()->assign('profil', $profil);
+        $profil = $profil->fetchAll(PDO::FETCH_ASSOC);   //Retourne le résultat de la requête dans un tableau associatif
+        Flight::view()->assign('profil', $profil);       //Initialise un élément profil, utile pour afficher les champs du tableau de session
         
         
         Flight::render("profil.tpl",array());
@@ -172,7 +167,7 @@ Flight::route('/profil', function(){
 
 
 Flight::route('/logout', function(){
-    session_unset();
+    session_unset();     //vide le tableau de session
     Flight::redirect('/');
 })
 ?>
