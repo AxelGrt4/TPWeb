cdce73552f854d516972352142ec587f75df5f67
AxelGrt4 <71839642+AxelGrt4@users.noreply.github.com>
2021-12-10 21:44:13 +0100

    Update index.php

--- tps/tp4/index.php
+++ tps/tp4/index.php
@@ -4,20 +4,21 @@ require '../../includes/flight-master/flight/Flight.php';
 require '../../includes/smarty-3.1.35/libs/Smarty.class.php';
 require '../../includes/pdo.php';
 
-session_start();
+session_start();                                  //Debut de la session php
 
-$pdo = new PDO(
+$pdo = new PDO(                                   //permet la connexion à la base de données
     "mysql:host = localhost;
      port=3306;dbname=m3104;charset=utf8",
      "root",
      "",
 );
 
-Flight::set('db', $pdo);
+Flight::set('db', $pdo);                       //Définit db comme variable globale de Flight, permettant l'accès à la bdd
+
 
 // Register Smarty as the view class
 // Also pass a callback function to configure Smarty on load
-Flight::register('view', 'Smarty', array(), function($smarty){
+Flight::register('view', 'Smarty', array(), function($smarty){       //Initialisation de smarty
     $smarty->template_dir = './templates/';
     $smarty->compile_dir = './templates_c/';
     $smarty->config_dir = './config/';
@@ -30,10 +31,10 @@ Flight::map('render', function($template, $data){
     
 });
 
-require "routes.php";
+require "routes.php";                              //Importe le ichier routes.php
 if(isset($_SESSION['Nom'])){
-    Flight::view()->assign('Nom', $_SESSION['Nom']);
+    Flight::view()->assign('Nom', $_SESSION['Nom']);    //Assigne à la variable globale 'Nom' la valeur de du champ 'Nom' du tableau de session, quand celui-ci est initialisé
     
 }
 
-Flight::start();
+Flight::start();               //Démarre le traitement Flight
